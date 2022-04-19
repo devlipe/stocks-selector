91dca64aa50515dbac92a966bef94572c670c81d
devlipe <felipe.p.ferreira@ufv.br>
2022-04-18 21:19:23 -0300

    fixes color if windows machine

--- src/config/config.go
+++ src/config/config.go
@@ -8,29 +8,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-const (
-	Reset = "\033[0m"
 
-	Black  = "\033[30m"
-	Red    = "\033[31m"
-	Green  = "\033[32m"
-	Yellow = "\033[33m"
-	Blue   = "\033[34m"
-	Purple = "\033[35m"
-	Cyan   = "\033[36m"
-	Gray   = "\033[37m"
-	White  = "\033[97m"
-
-	BackBlack  = "\033[40m"
-	BackRed    = "\033[41m"
-	BackGreen  = "\033[42m"
-	BackYellow = "\033[43m"
-	BackBlue   = "\033[44m"
-	BackPurple = "\033[45m"
-	BackCyan   = "\033[46m"
-	BackGray   = "\033[47m"
-	BackWhite  = "\033[107m"
-)
 
 func GenerateFlags() {
 	/* true */ FILTRAR_VOLUME_FINANC := flag.Bool("volfin", true, "Usado para filtrar Volume Financeiro de cada ação.")
