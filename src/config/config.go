0d9378732baa3f1fc1747583346379f94cba39d0
devlipe <felipe.p.ferreira@ufv.br>
2022-04-19 18:45:49 -0300

    Add config variables manipulation

--- src/config/config.go
+++ src/config/config.go
@@ -1,48 +1,199 @@
 package config
 
 import (
-	"flag"
+	"errors"
+	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/spf13/viper"
 )
 
+var (
+	_FILTRAR_VOLUME_FINANC bool = true
+	_FILTRAR_MARGEM_EBIT   bool = true
+	_FILTRAR_PL            bool = true
+	_FILTRAR_ROA           bool = true
 
+	_VOL_FIN_MIN        int     = 200000
+	_MARGEM_EBIT_MINIMA float64 = 0.0
+	_PL_MINIMO          float64 = 1.5
+	_ROA_MINIMO         float64 = 5.0
 
-func GenerateFlags() {
-	/* true */ FILTRAR_VOLUME_FINANC := flag.Bool("volfin", true, "Usado para filtrar Volume Financeiro de cada ação.")
-	/* true */ FILTRAR_MARGEM_EBIT := flag.Bool("mebit", true, "Usado para filtrar Margem Ebit de cada ação.")
-	/* true */ FILTRAR_PL := flag.Bool("pl", true, "Usado para filtrar Pl de cada ação.")
-	/* true */ FILTRAR_ROA := flag.Bool("roa", true, "Usado para filtrar Roa de cada ação.")
+	_PESO_PL      float64 = 1.5
+	_PESO_ROA     float64 = 1.0
+	_PESO_EV_EBIT float64 = 2.0
 
-	/* 200.000 */
-	VOL_FIN_MIN := flag.Int("vl-min", 200000, "Volume financeiro minimo, uma ação com volume financeiro baixo é pouco negociada na bolsa, e pode gerar dificuldades na hora da venda")
-	/* 0 */ MARGEM_EBIT_MINIMA := flag.Float64("mebit-min", 0.0, "Margem Ebit mininima, uma ação com margem ebit negativa diz que a empresa dá prejuízo, queremos retirá-las")
-	/* 1.5 */ PL_MINIMO := flag.Float64("pl-min", 1.5, "Pl minimo, quanto menor, melhor, entretanto valores pequenos podem indicar que os dados nao estao bons, geralmente utiliza-se valores entre 1 e 2")
-	/* 5 */ ROA_MINIMO := flag.Float64("roa-min", 5.0, "Roa minimo, medido em porcentagem, considera-se um roa alto, bom")
+	_OUTPUT string = "cli"
+)
 
-	/* 1.5 */
-	PESO_PL := flag.Float64("p-pl", 1.5, "Peso do Pl para rankear as ações")
-	/* 1 */ PESO_ROA := flag.Float64("p-roa", 1.0, "Peso do Roa para rankear as ações")
-	/* 2 */ PESO_EV_EBIT := flag.Float64("p-evebit", 2.0, "Peso do EvEbit para rankear as ações")
+var clear map[string]func()
 
-	/* cli */
-	OUTPUT := flag.String("out", "cli", "Fomato de arquivo a ser gerado na saida. Suporte para txt, xlsx, cli.")
-	flag.Parse()
+func init() {
+	clear = make(map[string]func())
 
-	viper.Set("FILTRAR_VOLUME_FINANC", *FILTRAR_VOLUME_FINANC)
-	viper.Set("FILTRAR_MARGEM_EBIT", *FILTRAR_MARGEM_EBIT)
-	viper.Set("FILTRAR_PL", *FILTRAR_PL)
-	viper.Set("FILTRAR_ROA", *FILTRAR_ROA)
-	viper.Set("VOL_FIN_MIN", *VOL_FIN_MIN)
-	viper.Set("MARGEM_EBIT_MINIMA", *MARGEM_EBIT_MINIMA)
-	viper.Set("PL_MINIMO", *PL_MINIMO)
-	viper.Set("ROA_MINIMO", *ROA_MINIMO)
-	viper.Set("PESO_PL", *PESO_PL)
-	viper.Set("PESO_ROA", *PESO_ROA)
-	viper.Set("PESO_EV_EBIT", *PESO_EV_EBIT)
-	viper.Set("OUTPUT", *OUTPUT)
+	clear["linux"] = func() {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
+func ConfigureApp() {
+	var configuring bool = true
+	var errorMessage string
+	for configuring {
+		printConfig(errorMessage)
+		fmt.Print(": ")
+
+		var choice string
+		var newValue string
+		fmt.Scanln(&choice, &newValue)
+		if choice == "" {
+			break
+		}
+
+		err := changeConfig(choice, newValue)
+		if err != nil {
+			errorMessage = err.Error()
+		} else {
+			errorMessage = ""
+		}
+	}
+	setEnvVariablesWithViper()
+}
+
+func changeConfig(choice, value string) error {
+	switch choice {
+	case "1":
+		_FILTRAR_VOLUME_FINANC = !_FILTRAR_VOLUME_FINANC
+	case "2":
+		_FILTRAR_MARGEM_EBIT = !_FILTRAR_MARGEM_EBIT
+	case "3":
+		_FILTRAR_ROA = !_FILTRAR_ROA
+	case "4":
+		_FILTRAR_PL = !_FILTRAR_PL
+	case "5":
+		temp, err := strconv.Atoi(value)
+		if err == nil && temp > 0 {
+			_VOL_FIN_MIN = temp
+		} else {
+			return errors.New("the value of Vol Min must be a integer and greater than 0")
+		}
+	case "6":
+		temp, err := strconv.ParseFloat(value, 64)
+		if err == nil {
+			_MARGEM_EBIT_MINIMA = temp
+		} else {
+			return errors.New("the value of minimun Ebit margin must be a number")
+		}
+	case "7":
+		temp, err := strconv.ParseFloat(value, 64)
+		if err == nil {
+			_ROA_MINIMO = temp
+		} else {
+			return errors.New("the value of minimum Roa must be a number")
+		}
+	case "8":
+		temp, err := strconv.ParseFloat(value, 64)
+		if err == nil {
+			_PL_MINIMO = temp
+		} else {
+			return errors.New("the value of minimum P/L must be a number")
+		}
+	case "9":
+		temp, err := strconv.ParseFloat(value, 64)
+		if err == nil && temp > 0 {
+			_PESO_EV_EBIT = temp
+		} else {
+			return errors.New("the value of Peso Ev/Ebit must be a number and greater than 0")
+		}
+	case "10":
+		temp, err := strconv.ParseFloat(value, 64)
+		if err == nil && temp > 0 {
+			_PESO_ROA = temp
+		} else {
+			return errors.New("the value of Peso Roa must be a number and greater than 0")
+		}
+	case "11":
+		temp, err := strconv.ParseFloat(value, 64)
+		if err == nil && temp > 0 {
+			_PESO_PL = temp
+		} else {
+			return errors.New("the value of Peso P/L must be a number and greater than 0")
+		}
+	case "12":
+		value = strings.ToLower(value)
+		if value == "cli" || value == "txt" || value == "xlsx" {
+			_OUTPUT = value
+		} else {
+			return errors.New("the value of output must be one of the following (cli, txt, xlxs)")
+		}
+	default:
+		return errors.New("something went wrong")
+	}
+	return nil
+}
+
+func printConfig(errorMessage string) {
+	ClearScreen()
+	fmt.Println("We need to set the variables that the program uses to generate the report.")
+	fmt.Println("Digit the config number follow by the new value to change it, than press enter to run!!")
+	fmt.Print("\nThe current configuration is:\n\n")
+	fmt.Println(Ternary(_FILTRAR_VOLUME_FINANC, Green, Red), "1. Filter Fin. Volume :\t", _FILTRAR_VOLUME_FINANC)
+	fmt.Println(Ternary(_FILTRAR_MARGEM_EBIT, Green, Red), "2. Filter Marg. Ebit :\t\t", _FILTRAR_MARGEM_EBIT)
+	fmt.Println(Ternary(_FILTRAR_ROA, Green, Red), "3. Filter Roa :\t\t", _FILTRAR_ROA)
+	fmt.Println(Ternary(_FILTRAR_PL, Green, Red), "4. Filter P/L :\t\t", _FILTRAR_PL)
+	fmt.Println(Cyan)
+	fmt.Println(" 5. Min Fin Volume :\t", _VOL_FIN_MIN)
+	fmt.Println(" 6. Min Ebit Margin :\t", _MARGEM_EBIT_MINIMA)
+	fmt.Println(" 7. Min Roa :\t\t", _ROA_MINIMO)
+	fmt.Println(" 8. Min P/L :\t\t", _PL_MINIMO)
+	fmt.Println(Yellow)
+	fmt.Println(" 9. Peso EV/Ebit :\t", _PESO_EV_EBIT)
+	fmt.Println(" 10. Peso Roa :\t\t", _PESO_ROA)
+	fmt.Println(" 11. Peso P/L :\t\t", _PESO_PL)
+	fmt.Println(Blue)
+	fmt.Println(" 12. Output mode : ", _OUTPUT)
+	if errorMessage != "" {
+		fmt.Println(Red)
+		fmt.Println(errorMessage)
+	}
+	fmt.Println(Reset)
+}
+
+func setEnvVariablesWithViper() {
+	viper.Set("FILTRAR_VOLUME_FINANC", _FILTRAR_VOLUME_FINANC)
+	viper.Set("FILTRAR_MARGEM_EBIT", _FILTRAR_MARGEM_EBIT)
+	viper.Set("FILTRAR_PL", _FILTRAR_PL)
+	viper.Set("FILTRAR_ROA", _FILTRAR_ROA)
+	viper.Set("VOL_FIN_MIN", _VOL_FIN_MIN)
+	viper.Set("MARGEM_EBIT_MINIMA", _MARGEM_EBIT_MINIMA)
+	viper.Set("PL_MINIMO", _PL_MINIMO)
+	viper.Set("ROA_MINIMO", _ROA_MINIMO)
+	viper.Set("PESO_PL", _PESO_PL)
+	viper.Set("PESO_ROA", _PESO_ROA)
+	viper.Set("PESO_EV_EBIT", _PESO_EV_EBIT)
+	viper.Set("OUTPUT", _OUTPUT)
+}
+
+func ClearScreen() {
+	value, ok := clear[runtime.GOOS]
+	if ok {
+		value()
+	} else {
+		fmt.Println("Your platform is unsupported! I can't clear terminal screen, sorry for the bad experience :(")
+	}
 }
 
 func HttpClient() *http.Client {
